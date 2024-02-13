c834aacff3a2c2b6257c82ec94633d873f228535
david-dev2050 <dyabroad@gmail.com>
2024-02-13 23:06:04 +0300

    jala media initial

---
+++ jala_app/static/jala_app/js/cookies.js
@@ -0,0 +1,52 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (!getCookie('cookieConsent')) {
+        showBanner();
+    }
+
+    document.getElementById('acceptCookies').addEventListener('click', function() {
+        setCookie('cookieConsent', 'accepted', 365); // Set a cookie named 'cookieConsent' with a one-year expiration
+        hideBanner();
+    });
+
+    document.getElementById('rejectCookies').addEventListener('click', function() {
+        setCookie('cookieConsent', 'rejected', 365);
+        hideBanner();
+    });
+
+    function setCookie(name, value, days) {
+        var expires = '';
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = '; expires=' + date.toUTCString();
+        }
+        document.cookie = name + '=' + value + expires + '; path=/';
+    }
+
+    function getCookie(name) {
+        var nameEQ = name + '=';
+        var cookies = document.cookie.split(';');
+        for(var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            while (cookie.charAt(0) == ' ') {
+                cookie = cookie.substring(1, cookie.length);
+            }
+            if (cookie.indexOf(nameEQ) == 0) {
+                return cookie.substring(nameEQ.length, cookie.length);
+            }
+        }
+        return null;
+    }
+
+    function showBanner() {
+        document.getElementById('cookieBanner').style.display = 'block';
+    }
+
+    function hideBanner() {
+        document.getElementById('cookieBanner').style.display = 'none';
+    }
+});
+
+
+
---
+++ jala_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import  login, logout, authenticate
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import *
+from django.db.models import Q
+from django.contrib.auth.hashers import make_password
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404
+import random, string
+from urllib.parse import urlencode
+from django.utils import timezone
+from django.urls import reverse
+from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
+
+# Create your views here.
+
+class SiteRequirements():
+    def __init__(self):
+        pass
+
+    def random_func(self, length):
+        return ''.join([random.choice(string.ascii_letters + string.digits) for i in range (length)])
+    
+    def Time(self):
+        return timezone.datetime.now().date()
+    
+class RenderUrls(SiteRequirements):
+    def render_view(self, request, view_file, **kwargs):
+        return render(request, view_file, kwargs)
+
+    def redirection_func(self, redirect_path=None, **kwargs):
+        if kwargs:
+            redirect_path += '?' + urlencode(kwargs)
+        return HttpResponseRedirect(reverse(redirect_path))
+    
+object = RenderUrls()
+
+def index_page(request):
+    time = object.Time()
+    return object.render_view(request, view_file='jala_app/index.html',time=time)
