7dab308e6348d4ed988edac21c5bf831dcf0a01d
david-dev2050 <dyabroad@gmail.com>
2024-02-28 09:55:35 +0300

    changed few things

--- jala_app/urls.py
+++ jala_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import *
 
@@ -12,3 +14,5 @@ urlpatterns =[
     path('our-service', services_page, name='services_page'),
     path('contact-us', contact_us_page, name='contact_us_page'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
